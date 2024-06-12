4947db06faa180582d0b3d796820b244aebc78c6
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-06-12 12:56:33 +0200

    hej test

--- src/main/java/kea/exercise/practice_animal_backend/PracticeAnimalBackendApplication.java
+++ src/main/java/kea/exercise/practice_animal_backend/PracticeAnimalBackendApplication.java
@@ -2,12 +2,18 @@ package kea.exercise.practice_animal_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class PracticeAnimalBackendApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(PracticeAnimalBackendApplication.class, args);
+        System.out.println("Hello World");
     }
+
+
+
 
 }
